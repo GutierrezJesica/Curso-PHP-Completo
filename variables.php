74cba31373e8208dd890d7316ff52b86ad521438
Gutierrez Jesica <jesica89@gmail.com>
2025-03-05 15:26:55 -0300

    creación de array

--- variables.php
+++ variables.php
@@ -1,6 +1,6 @@
 <?php
 
-/* DECLARAR VARIABLES */
+# DECLARAR VARIABLES
 
 $nombre = "Fernando";
 
@@ -8,10 +8,47 @@ $nombre = "Rocio";
 
 echo $nombre;
 
-/* DECLARAR CONSTANTES */
+# DECLARAR CONSTANTES
 
 const apellido = "Dominguez";
 
 define("deportes", array("tenis", "futbol", "baloncesto"));
 
 echo deportes[2];
+
+# DECLARACIÓN DE ARRAY
+
+$estudiantes = array("Fernando", "Rocio", "Diana");
+
+echo $estudiantes[2]="Jesica"; // asi se remplaza un dato del array por otro luego del =
+
+# ARRAY DE TIPO ESCALAR
+
+$estudiantes =["lola", "lolo", "loli"];
+
+#ARRAY DE TIPO ASOCIATIVO
+
+$tutor = [
+    "nombre" => "Fernando",
+    "apellido" => "Dominguez",
+    "edad" => 30
+];
+
+echo $tutor["edad"];
+
+#ARRAY DE MULTIPLES DIMENSIONES
+
+$tutor_2 = [
+    "nombre" => "Jazmin",
+    "apellido" => "Rojas",
+    "edad" => 30,
+    "cursos" => ["PHP", "HTML", "CSS"]
+];
+
+$tutor_2["pais"] = "Colombia";
+
+echo $tutor_2["pais"];
+
+echo count($estudiantes);
+
+echo count($tutor_2, COUNT_RECURSIVE); // cuenta de manera recursiva
