2d06f894a11dda712425a027bd11ad17012674a7
Gutierrez Jesica <jesica89@gmail.com>
2025-03-06 11:52:07 -0300

    fin por hoy, video 24

---
+++ estructuras-condicionales.php
@@ -0,0 +1,69 @@
+<!-- <!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    if (0 >= 3) { ?>
+        <h1>Verdadero</h1>
+    <?php }else { ?>
+        <h1>Falso</h1>
+    <?php }
+    ?>
+
+</body>
+</html> -->
+
+<?php
+
+/* $edad = 21;
+
+if ($edad >= 18) {
+    echo "Eres mayor de edad";
+} else {
+    echo "Eres menor de edad";
+} */
+
+//-----------------------------------------
+
+/* $total = 120;
+
+if($total > 100) {
+    $total = $total - ($total * 0.20); // si total es mayor a 100 tiene un descuento del 20%
+}
+
+echo "El total es de: $" . $total; */
+
+//----------------------------------------- OPERADOR TERNARIO
+
+/* $total = (2 > 7) ? 10 * 7 : 10 * 5;
+
+echo $total; */
+
+/* $camisas = 2;
+$precio = 10;
+
+$total = $precio * $camisas;
+
+$total = ($camisas >= 3) ? $total - ($total * 0.20) : $total - ($total * 0.10);
+
+echo "El total a pagar es de: $" . $total; */
+
+//--------------------------------------- ANIDADAS
+
+$edad = 63;
+$genero = "M";
+
+if ($genero == "M") {
+    if($)
+}elseif ($genero == "F") {
+
+} else {
+    echo "Genero no valido";
+}
+
