c9e4df9e96178977b88d58dbe7196c7429360b66
Gutierrez Jesica <jesica89@gmail.com>
2025-03-11 13:56:01 -0300

    index funcionando y agregado css

--- Programa/include.php
+++ Programa/include.php
@@ -4,3 +4,8 @@
         ["codigo" => "#A00002", "nombre" => "Verde-Esmeralda"],
         ["codigo" => "#A00003", "nombre" => "Azul-Peltre"]
     ];
+
+    foreach($colores as $cod) {
+        echo $cod["codigo"]. "<br>";
+        echo $cod["nombre"]. "<br>";
+    }
--- Programa/index.php
+++ Programa/index.php
@@ -3,20 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="stilos.css">
     <title>Document</title>
 </head>
 <body>
-
-    <?php
-
-    require "include.php";
-
-    foreach($colores as $cod) {
-        echo $cod["codigo"]. "<br>";
-        echo $cod["nombre"]. "<br>";
-    };
-    ?>
-
+    <h1 class="modulo">HTML Y PHP</h1>
+        <div class="contenido">
+            <?php require "include.php"; ?>
+        </div>
 </body>
 </html>
 
