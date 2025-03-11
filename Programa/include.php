59929fb45f59c2863a3db13c1b8aa8ca42e00302
Gutierrez Jesica <jesica89@gmail.com>
2025-03-11 12:19:37 -0300

    programa muestra index.php correctamente

---
+++ Programa/include.php
@@ -0,0 +1,6 @@
+<?php
+    $colores = [
+        ["codigo" => "#A00001", "nombre" => "Rojo-Ruby"], // array anidado en otro array
+        ["codigo" => "#A00002", "nombre" => "Verde-Esmeralda"],
+        ["codigo" => "#A00003", "nombre" => "Azul-Peltre"]
+    ];
