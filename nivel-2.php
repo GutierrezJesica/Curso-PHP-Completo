f505281a300d9bfaf555f1a79c89aba3c28922e2
Gutierrez Jesica <jesica89@gmail.com>
2025-05-15 15:26:55 -0300

    Clave incriptada y verificada

--- nivel-2.php
+++ nivel-2.php
@@ -31,5 +31,12 @@ echo $dia_actual." ".$fecha_actual. "<br>";
 
 $clave = "HannieSalvaje123";
 
-echo password_hash($clave, PASSWORD_DEFAULT);
+$clave_procesada = password_hash($clave, PASSWORD_BCRYPT, ["cost"=>11]);
 
+$clave_2 = "ChimueloSalvaje_2324";
+
+if(password_verify($clave_2, $clave_procesada)) {
+    echo "Las claves coinciden";
+}else{
+    echo "Las claves no coinciden";
+}
