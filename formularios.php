3a0068f1546b23ec80bee7224d9161d03d1069b2
Gutierrez Jesica <jesica89@gmail.com>
2025-06-23 14:50:54 -0300

    inicio de formulario

---
+++ formularios.php
@@ -0,0 +1,32 @@
+<?php
+# ------------------------------- ENVIAR FORMULARIOS CON GET Y POST
+?>
+
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form>
+        <div>
+            <label for="nombre">Nombre</label>
+            <input type="text" name="nombre" id="nombre">
+        </div>
+
+        <br>
+
+        <label for="apellido">Apellido</label>
+        <input type="text" name="apellido" id="apellido">
+
+        <br>
+
+        
+
+    </form>
+</body>
+</html>
+
